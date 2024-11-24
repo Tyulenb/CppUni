aec598f4b713e4b7572a5b3c4e8d7594f69535f8
Igor Voronkov <115886661+Tyulenb@users.noreply.github.com>
2024-11-24 20:02:03 +0000

    Add task 12

---
+++ task12.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <vector>
+#include <mutex>
+#include <chrono>
+
+using namespace std;
+
+mutex mute;
+vector<int> found;
+vector<int> big_data;
+
+void number_search(int start, int end, int num) {
+	for (int i = start; i < end; i++) {
+		if (big_data[i] == num) {
+			mute.lock();
+			found.push_back(i);
+			mute.unlock();
+		}
+	}
+}
+
+int main()
+{
+	srand(123213);
+	int size = 100000000;
+	for (long long i = 0; i < size; i++) {
+		big_data.push_back(rand()% 2147483647 +1);
+	}
+
+	cout << "Enter the number you want to find in the BIG DATA\n";
+	int num; cin >> num;
+
+	thread t1(number_search, 0, size/4, num);
+	thread t2(number_search, size/4, size/2, num);
+	thread t3(number_search, size / 2, size/4*3, num);
+	thread t4(number_search, size / 4 * 3, size, num);
+	auto st = chrono::high_resolution_clock::now();
+	t1.join();
+	t2.join();
+	t3.join();
+	t4.join();
+
+	cout << found.size() << "\n";
+	auto en = chrono::high_resolution_clock::now();
+	chrono::duration<double> duration = en - st;
+	cout << "It took " << duration.count() << " seconds\n";
+}
